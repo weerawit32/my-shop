b12e426d4090965140e1e6c8104289cdc9013f4a
weerawit32 <vasthabo@hotmail.com>
2023-02-05 17:57:11 +0700

    config and package josn

--- my-shop/next.config.js
+++ my-shop/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.hm.com",
+        port: "",
+        pathname: "/assets/hm/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
